525cf9657800e381aacbb31ec260c83cb3b11b1e
Geoffrey Cleaves <gcleaves@gcleaves.com>
2023-11-21 22:39:24 +0100

    better docs

--- components/Docs.vue
+++ components/Docs.vue
@@ -4,26 +4,30 @@
 
 <template>
   <v-container>
-  <h1 id="documentation">Documentation</h1>
+    <h1 id="documentation">Documentation</h1>
 
-  <h2 id="getting-started">Getting started</h2>
-  <h3 id="recommended">Recommended</h3>
+    <h2 id="getting-started">Getting started</h2>
+    <h3 id="recommended">Recommended</h3>
 
-  <ol>
-    <li><a href="https://theoephraim.github.io/node-google-spreadsheet/#/guides/authentication?id=service-account">Create a service account</a></li>
-    <li>Share a Google Sheet with the Service Account email address.</li>
-    <li>Log in here with Google.</li>
-    <li>Go to Settings and add your Google Service Account JSON.</li>
-    <li>Go to this app’s Sheets section and click New. Add the Google Sheet ID and save.</li>
-    <li>Visit the newly created API endpoint. You should see your Google Sheet in JSON format.</li>
-  </ol>
+    <ol>
+      <li><a href="https://theoephraim.github.io/node-google-spreadsheet/#/guides/authentication?id=service-account">Create
+        a service account</a></li>
+      <li>Share a Google Sheet with the Service Account email address.</li>
+      <li>Log in here with Google.</li>
+      <li>Go to Settings and add your Google Service Account JSON.</li>
+      <li>Go to this app’s Sheets section and click New. Add the Google Sheet ID and save.</li>
+      <li>Visit the newly created API endpoint. You should see your Google Sheet in JSON format.</li>
+    </ol>
 
-  <h3 id="alternate1">Alternate 1</h3>
-  <ol>
-    <li>Share your Google Sheet with <a href="mailto:sheets-api-service-account@sheets-api-402810.iam.gserviceaccount.com">sheets-api-service-account@sheets-api-402810.iam.gserviceaccount.com</a> .</li>
-    <li>Go to this app’s Sheets section and click New. Add the Google Sheet ID and save.</li>
-    <li>Visit the newly created API endpoint. You should see your Google Sheet in JSON format.</li>
-  </ol>
+    <h3 id="alternate1">Alternate 1</h3>
+    <ol>
+      <li>Share your Google Sheet with <a
+          href="mailto:sheets-api-service-account@sheets-api-402810.iam.gserviceaccount.com">sheets-api-service-account@sheets-api-402810.iam.gserviceaccount.com</a>
+        .
+      </li>
+      <li>Go to this app’s Sheets section and click New. Add the Google Sheet ID and save.</li>
+      <li>Visit the newly created API endpoint. You should see your Google Sheet in JSON format.</li>
+    </ol>
 
     <h3 id="alternate2">Alternate 2</h3>
     <ol>
@@ -34,13 +38,70 @@
 
 
     <h2 id="advanced">Advanced</h2>
+    <h3>Authentication</h3>
+    <p>If you've assigned an API key, place it into the <code>x-api-key</code> header.
+      Alternately, you can put it in the <code>_x-api-key</code> search parameter. Note the underscore.
+    </p>
 
-  <h3 id="search">Search</h3>
-    <p><code>/api/sheet/{sheetuid}/search?search1=param1&etc</code></p>
+    <h3 id="search">Search</h3>
+    <p><code>GET /api/sheet/{sheetuid}/search?search1=param1&searchN=paramN</code></p>
     <h4>Examples</h4>
-    <p>Find rows with id > 5 and c = 9: /api/sheet/{sheetuid}/search?id=>5&c=9</p>
+    <p>Find rows with id > 5 and c = 9: <code>GET /api/sheet/{sheetuid}/search?id=>5&c=9</code></p>
+    <p>Find rows with id >= 3 and id &lt; 6: <code>GET /api/sheet/{sheetuid}/search?id=>=3&id=&lt;6</code></p>
 
-  <h3 id="update">Update</h3>
+    <h3 id="update">Update</h3>
+    <p><code>PATCH /api/sheet/{sheetuid}</code></p>
+    <h4>Example</h4>
+    <p>Update rows with id >= 3 and id &lt; 6</p>
+    <p>Body:</p>
+    <code><pre>
+{
+  "query": {
+    "logic": "and", // "and" is the default logic
+    "rules": {
+      "id": [">=3","&lt;6"]
+    }
+  },
+  "update": {
+    "column_b": "value_b",
+    "column_c": "value_c",
+  }
+}
+    </pre></code>
+
+    <h3 id="delete">Delete</h3>
+    <p><code>DELETE /api/sheet/{sheetuid}</code></p>
+    <h4>Example</h4>
+    <p>Update rows with id >= 3 and id &lt; 6</p>
+    <p>Body:</p>
+    <code><pre>
+{
+  "query": {
+    "logic": "and", // "and" is the default logic
+    "rules": {
+      "id": [">=3","&lt;6"]
+    }
+  }
+}
+    </pre></code>
+
+    <h3 id="insert">Insert</h3>
+    <p><code>PUT /api/sheet/{sheetuid}</code></p>
+    <h4>Example</h4>
+    <p>Body:</p>
+    <code><pre>
+{
+  "insert": [{
+     "column_a": "value_a",
+     "column_b": "value_b",
+  },{
+     "column_a": "value_a",
+     "column_b": "value_b",
+  }]
+}
+    </pre></code>
+
+
   </v-container>
 </template>
 
@@ -48,11 +109,13 @@
 ol, ul {
   padding-left: 1em;
 }
-h2,h3,h4 {
+
+h2, h3, h4 {
   margin-bottom: 5px;
-  margin-top: 15px;
+  margin-top: 10px;
 }
+
 p, h1 {
-  padding-bottom: 15px;
+  padding-bottom: 10px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -8,9 +8,14 @@ definePageMeta({
 <template>
   <v-container>
   <h1 id="free-read-and-write-api-access-to-google-sheets">Free Read and Write API Access to Google Sheets</h1>
-  <p>Use this free service to convert your Google Sheets into a simple REST<br>
-    API. To ensure absolute privacy, host the <a href="https://github.com/gcleaves/sheet-api-be">open source software</a> yourself.</p>
-  <p>There are plenty of alternatives to this software, however few of them give you unlimited usage and include the ability to update and search Google Sheets. Commercial services do provide update ability, but you must hand over complete access to all your Google Sheets to a complete stranger.</p>
+  <p>Use this free service to convert your Google Sheets into a simple REST API.
+    To ensure absolute privacy, host the <a href="https://github.com/gcleaves/sheet-api-be">open source software</a>
+    yourself.
+  </p>
+  <p>There are plenty of alternatives to this software, however few of them give you unlimited usage and
+    include the ability to update and search Google Sheets. Commercial services do provide update ability,
+    but you must hand over complete access to all your Google Sheets to a complete stranger.
+  </p>
   </v-container>
 </template>
 
