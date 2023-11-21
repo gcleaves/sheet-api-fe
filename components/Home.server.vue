506e6e200ce0050dad5a2dc4697970d1e6e03a07
root <root@vps-8b568f1f.vps.ovh.net>
2023-11-21 09:20:06 +0000

    remove path

--- components/Home.server.vue
+++ components/Home.server.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
-import markdown from '@wcj/markdown-to-html';
-import * as fs from 'node:fs';
-import {join} from 'node:path';
 
-const path = join(__dirname,'home.md');
-const h = markdown(fs.readFileSync(path).toString());
 
 </script>
 
 <template>
     <v-container>
-      <div v-html="h"></div>
     </v-container>
 </template>
 
